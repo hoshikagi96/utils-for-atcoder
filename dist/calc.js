c6036a89e07816faabb8bc13a0056bb4d45ac045
hoshikagi96 <hoshikagi96@gmail.com>
2022-02-05 22:56:21 +0900

    first commit

---
+++ dist/calc.js
@@ -0,0 +1,86 @@
+"use strict";
+// 素数判定: √num　までの数について一つずつ調べる
+function isPrime(num) {
+    for (let i = 2; i * i <= num; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+//　約数列挙：　全ての約数を配列で取得　（1, numを含む）
+function getAllDivisor(num) {
+    const nums = [];
+    // numを含みたくない場合は i = 2から開始
+    for (let i = 1; i * i <= num; i++) {
+        if (num % i === 0) {
+            nums.push(i);
+            if (num / i !== i) {
+                nums.push(num / i);
+            }
+        }
+    }
+    return nums;
+}
+// 素因数分解：　素因数分解の答えを配列で取得　（数が小さい順）
+function getPrimeFactors(num) {
+    const ans = [];
+    if (num === 1) {
+        ans.push(1);
+        return ans;
+    }
+    for (let i = 2; i * i <= num; i++) {
+        while (num % i === 0) {
+            ans.push(i);
+            num = num / i;
+        }
+    }
+    if (num !== 1) {
+        ans.push(num);
+    }
+    return ans;
+}
+// 最大公約数：　ユークリッド互除法（A*Bの長方形から、短辺に合わせた長方形を取り除いていく） Gratest Common Divisor
+function getGCD(numA, numB) {
+    const f = (m, n) => n ? f(n, m % n) : m;
+    return f(numA, numB);
+}
+// *複数の数の最大公約数
+function getGCDOfAll(N, nums) {
+    const f = (m, n) => n ? f(n, m % n) : m;
+    let lastRes = 0;
+    for (let i = 0; i < N; i++) {
+        lastRes = f(nums[i], lastRes);
+    }
+    return lastRes;
+}
+// 最小公倍数
+// 組み合わせ：　n個のものからr個並べる(nPr)  TODO: 効率化できそう
+function getPermutation(n, r) {
+    let a = 1;
+    for (let i = 1; i <= n; i++) {
+        a = a * i;
+    }
+    let b = 1;
+    for (let i = 1; i <= n - r; i++) {
+        b = b * i;
+    }
+    return a / b;
+}
+// 組み合わせ：　n個のものからr個選ぶ(nPr)  TODO: 効率化できそう
+function getCombination(n, r) {
+    let a = 1;
+    for (let i = 1; i <= n; i++) {
+        a = a * i;
+    }
+    let b = 1;
+    for (let i = 1; i <= n - r; i++) {
+        b = b * i;
+    }
+    let c = 1;
+    for (let i = 1; i <= r; i++) {
+        b = b * i;
+    }
+    return a / (b * c);
+}
+console.log(getCombination(10, 3));
